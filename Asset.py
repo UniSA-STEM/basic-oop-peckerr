895473ddf864e2eefb48dccf66574731039ac301
peckerr <162071537+peckerr@users.noreply.github.com>
2025-10-16 20:37:32 +1100

    Created basic class for asset.py, containing name, description, and encryption states.

--- Asset.py
+++ Asset.py
@@ -1,8 +1,27 @@
 """
 File: Asset.py
-Description: <A brief description of this Python module.>
-Author: <full name>
-ID: <student_id>
-Username: <username>
+Description: A class that represents an asset, to be moved, consumed, or used in various actions taken via other .py files.
+Author: Thomas Cochrane
+ID: 110466784
+Username: COCTY007
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.encrypted = False              # Base state assets are not encrypted
+
+    def encrypt(self):
+        self.encrypted = True
+
+    def decrypt(self):
+        self.encrypted = False
+
+    def __str__(self):
+        if self.encrypted:
+            return f'{self.name} {self.description} [Encrypted]'
+        else:
+            return f'{self.name} {self.description}'
+
